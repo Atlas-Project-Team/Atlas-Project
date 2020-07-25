8e2d0cb37ef7a4d864b9d50a7282395ea2d86770
Ben Lamont <benjamin.m.lamont@gmail.com>
2020-07-25 11:19:08 +1000

    Capitalisation issue maybe?

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three";
-import {OrbitControls} from "../node_modules/three/examples/jsm/controls/OrbitControls";
-import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
-import {DRACOLoader} from '../node_modules/three/examples/jsm/loaders/DracoLoader';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader';
 import Vue from 'vue';
 import Fuse from 'fuse.js';
 import * as firebase from 'firebase/app';
