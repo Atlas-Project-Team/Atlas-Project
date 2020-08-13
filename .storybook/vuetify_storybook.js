7e7dbae814f90c7e13d662f73c5ef72cfcd722d1
Ben Lamont <benjamin.m.lamont@gmail.com>
2020-08-13 22:47:05 +1000

    I did a thing with the small objects component and it looks pretty neat

--- .storybook/vuetify_storybook.js
+++ .storybook/vuetify_storybook.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'; // loads all components
 import 'vuetify/dist/vuetify.min.css'; // all the css for components
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
--- src/components/objects/objects.small.vue
+++ src/components/objects/objects.small.vue
@@ -1,7 +1,91 @@
 <template>
-    <div style="width: 600px; height: 450px; background-color: darkgray;">
-        <v-text-field v-model="searchQuery"></v-text-field>
-        {{searchQuery}}
+    <v-container fluid style="width: 33vw; height: 50vh; background-color: #343a40">
+        <h4 style="color: white; text-align: right">Objects</h4>
+        <v-text-field
+            v-model="searchQuery"
+            placeholder="Search"
+            dense
+            dark
+            ></v-text-field>
+        <v-row dense>
+            <v-col cols="5">
+                <v-select
+                        v-model="currentFilter"
+                        :items="filters"
+                        label="Select a property to filter"
+                        outlined
+                        dense
+                        dark
+                ></v-select>
+            </v-col>
+            <v-col cols="4">
+                <v-select
+                        v-model="currentFilterValue"
+                        :items="getFilters(currentFilter)"
+                        label="Select a value to filter"
+                        outlined
+                        dense
+                        dark
+                ></v-select>
+            </v-col>
+            <v-col cols="2">
+                <v-btn outlined dark v-on:click="createFilter(currentFilter, currentFilterValue)">Apply</v-btn>
+            </v-col>
+        </v-row>
+        <v-row dense v-if="Object.keys(currentFilters).length>0">
+            <v-chip
+                    v-for="(value, filter) in currentFilters"
+                    :key="`${filter}:${value}`"
+                    class="ml-2 mr-2"
+                    close
+                    @click:close="deleteFilter(filter)"
+            >{{filter}}: {{value}}</v-chip>
+        </v-row>
+        <v-row style="color: white">
+            <v-col>
+                <v-list flat dark dense style="background-color: #343a40">
+                    <v-list-item-group
+                        v-model="currentlySelected"
+                        color="blue lighten-3">
+                        <v-list-item
+                                v-bind:item="item"
+                                v-bind:key="item.objectId"
+                                :map-data="mapData"
+                                v-for="item in mapData"
+                                v-if="(searchResults.includes(item.objectId.toString()) || searchQuery.trim()==='') && checkFilter(item)">
+                            <v-list-item-content>
+                                <v-list-item-title v-text="item.name"></v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </v-col>
+            <v-col>
+                <p v-if="currentItem===undefined">No map item currently selected. <br/>Select an item from the list to view its details.</p>
+                <v-card v-else dark>
+                    <v-card-title>{{currentItem.name}}</v-card-title>
+                    <v-card-text>
+                        <v-list dense>
+                            <v-subheader>Object Details</v-subheader>
+                            <v-list-item
+                                v-for="(property, key) in currentItem.objectInfo"
+                                :key="key"
+                                style="height: 20px; min-height: 20px;">
+                                <strong>{{key}}</strong>: {{property}}
+                            </v-list-item>
+                        </v-list>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                                text
+                                color="blue lighten-4"
+                        >
+                            Look At
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
             <!--
             <div class="input-group mb-1">
                 <input class="form-control text-white bg-dark border-light" id="search" onsubmit="" placeholder="Search"
@@ -39,7 +123,7 @@
                 ></map-item>
             </div>
             -->
-    </div>
+    </v-container>
 </template>
 
 <script>
@@ -60,10 +144,14 @@
                 searchQuery: "",
                 currentFilter: "Filter",
                 currentFilterValue: "Value",
-                currentFilters: {}
+                currentFilters: {},
+                currentlySelected: undefined
             }
         },
         computed: {
+            currentItem: function() {
+                return this.mapData[this.currentlySelected];
+            },
             searchResults: function () {
                 return this.fuse.search(this.searchQuery).map((value) => {
                     return value.item.objectId.toString()
